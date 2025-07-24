3cc96a67ff1749e5819a389ca26390ff707f0e16
Manu Masson <manummasson8@gmail.com>
2025-07-24 17:45:30 +0200

    tests

--- backend/tests/integration_tests/agentic_workflows/agents_and_prompts_tests/tree_action_decider/test_tree_action_decider_integration.py
+++ backend/tests/integration_tests/agentic_workflows/agents_and_prompts_tests/tree_action_decider/test_tree_action_decider_integration.py
@@ -61,6 +61,7 @@ class TestTreeActionDeciderIntegration:
             summary="REST API design decisions",
             parent_id=1
         )
+        api_node.relationships[1] = "is an implementation detail of"
         tree.tree[2] = api_node
         arch_node.children.append(2)
         
@@ -72,6 +73,7 @@ class TestTreeActionDeciderIntegration:
             summary="Database architecture choices",
             parent_id=1
         )
+        db_node.relationships[1] = "is a component of"
         tree.tree[3] = db_node
         arch_node.children.append(3)
         
