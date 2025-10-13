efc2121ab058ec41c6ede5198cdfd9d2fc08a82d
Manu Masson <manummasson8@gmail.com>
2025-10-13 23:13:33 +1100

    get cola

---
+++ frontend/webapp/src/graph-core/graphviz/layout/raf.ts
@@ -0,0 +1,23 @@
+interface WindowWithPrefixes extends Window {
+  webkitRequestAnimationFrame?: (callback: FrameRequestCallback) => number;
+  mozRequestAnimationFrame?: (callback: FrameRequestCallback) => number;
+  msRequestAnimationFrame?: (callback: FrameRequestCallback) => number;
+}
+
+let raf;
+
+if( typeof window !== typeof undefined ){
+  const win = window as unknown as WindowWithPrefixes;
+  raf = ( window.requestAnimationFrame ||
+    win.webkitRequestAnimationFrame ||
+    win.mozRequestAnimationFrame ||
+    win.msRequestAnimationFrame ||
+    ((fn: FrameRequestCallback) => setTimeout(fn, 16))
+  );
+} else { // if not available, all you get is immediate calls
+  raf = function( cb: FrameRequestCallback ){
+    cb(0);
+  };
+}
+
+export default raf;
