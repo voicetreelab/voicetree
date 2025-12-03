82e1f3f158c6ebe07fa62881b5d2df2eecdcc091
Manu Masson <manummasson8@gmail.com>
2025-12-03 15:55:36 +1100

    expand env vars

--- frontend/webapp/src/pure/settings/index.ts
+++ frontend/webapp/src/pure/settings/index.ts
@@ -1,2 +1,4 @@
 export type { VTSettings, AgentConfig, EnvVarValue } from './types';
-export { DEFAULT_SETTINGS, resolveEnvVars } from './types';
+export { DEFAULT_SETTINGS } from './types';
+export {expandEnvVarsInValues} from "@/pure/settings/resolve-environment-variable";
+export {resolveEnvVars} from "@/pure/settings/resolve-environment-variable";
