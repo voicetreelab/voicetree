ddf50f38ffb9c868996d6d9155d1673cebf443ba
Manu Masson <manummasson8@gmail.com>
2026-01-12 11:38:36 +1100

    extract settings

--- frontend/webapp/src/pure/settings/index.ts
+++ frontend/webapp/src/pure/settings/index.ts
@@ -1,4 +1,5 @@
 export type { VTSettings, AgentConfig, EnvVarValue } from './types';
-export { DEFAULT_SETTINGS, AGENT_NAMES, getRandomAgentName } from './types';
+export { AGENT_NAMES, getRandomAgentName } from './types';
 export {expandEnvVarsInValues} from "@/pure/settings/resolve-environment-variable";
 export {resolveEnvVars} from "@/pure/settings/resolve-environment-variable";
+export {DEFAULT_SETTINGS} from "@/pure/settings/DEFAULT_SETTINGS";
