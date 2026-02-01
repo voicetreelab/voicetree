ee433f5121dc104e76b0bd943973a4a2f803d484
Manu Masson <manummasson8@gmail.com>
2026-02-01 19:46:13 +1100

    More node spacing in layout

--- webapp/src/shell/UI/cytoscape-graph-ui/graphviz/layout/autoLayout.ts
+++ webapp/src/shell/UI/cytoscape-graph-ui/graphviz/layout/autoLayout.ts
@@ -48,7 +48,7 @@ const DEFAULT_OPTIONS: AutoLayoutOptions = {
   animate: true,
   maxSimulationTime: 2000,
   avoidOverlap: true,
-  nodeSpacing: 10,
+  nodeSpacing: 70,
   handleDisconnected: true, // handles disconnected components
   convergenceThreshold: 0.4,
   unconstrIter: 15, // TODO SOMETHINIG ABOUT THIS IS VERY IMPORTANT LAYOUT BREAK WITHOUT
